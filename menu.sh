2a2e0d88205e7012789ec384f72b9f3d6d188a8c
surinvpn <45985719+surinvpn@users.noreply.github.com>
2019-04-23 13:30:01 +0700

    Add files via upload

---
+++ menu.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+#Menu
+clear
+echo -e " "
+echo -e "\033[01;32m  }~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{"
+echo -e "  {    Wallcom to server Debian7-8    }"
+echo -e "  }~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{"
+echo -e "  { Script mod by ADMIN-SPNET  }"
+echo -e "  }~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{"
+echo -e "  {   prin { menu } Show menu items   }"
+echo -e "  }~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{"
+echo -e " "
+echo -e "\033[1;31m { a } เพิ่มชื่อผู้ใช้งาน Create SSH & OpenVPN"
+echo -e "\033[1;32m"
